f21a6dcaa8edddab24c2f5ff0c9f00e8edb83860
Robin Kamaliev <robinkamaliev@MacBook-Pro-Robin.local>
2023-06-02 12:14:34 +0500

    add Log:info  in /AmoCRM/StoreController (logs in fail)

--- app/Http/Controllers/AmoCRM/StoreController.php
+++ app/Http/Controllers/AmoCRM/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\AmoCRM;
 
 use App\Http\Requests\AmoCRM\StoreRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class StoreController extends BaseController
 {
@@ -14,7 +15,7 @@ class StoreController extends BaseController
 
         $this->service->MethodLeadsComplex($data); // Отправка данных в АмосСРМ
         $this->service->MethodTasks($data); // Добавление задачи в АмоСРМ
-
+        Log::info('Создана сделка, добавлен контакт, добавлена задача: Имя ' . $data['name'].', телефон: '.$data['tel']); // Логирование
         return view('amocrm.index');
     }
 }
