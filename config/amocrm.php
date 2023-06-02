884901d9732dfbe664fbacb98c2177679bc168dd
Robin Kamaliev <robinkamaliev@MacBook-Pro-Robin.local>
2023-06-02 11:36:46 +0500

    add config AmoCRM

---
+++ config/amocrm.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'token' => file_get_contents('../tokenAmoCRM.txt'),
+    'client_id' => '***',
+    'client_secret' => '***',
+    'code' => '***',
+    'redirect_uri' => '***',
+    'domain' => '***',
+];
+
